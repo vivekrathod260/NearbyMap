a3ea9811dad350133aff12d60de88e7f20260978
vivekrathod260 <vivek.rathod@walchandsangli.ac.in>
2026-06-09 02:00:37 +0530

    Migration init on startup + Added migrations + Separated migrations tracking on same DB for both services

--- BusinessService/Program.cs
+++ BusinessService/Program.cs
@@ -12,7 +12,14 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(
 );
 
 builder.Services.AddDbContext<BusinessDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sql => sql.EnableRetryOnFailure(3))
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlServer"),
+        sqlServerOptions =>
+        {
+            sqlServerOptions.EnableRetryOnFailure(3);
+            sqlServerOptions.MigrationsHistoryTable("__BusinessMigrationsHistory");
+        }
+    )
 );
 
 var app = builder.Build();
